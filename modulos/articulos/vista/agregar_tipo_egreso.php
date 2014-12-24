66d9d40fde52d52867591f1995be16e18435ecd9
jaimematsuda <jaimematsuda@gmail.com>
2014-12-24 00:36:56 -0500

    tipologia el modelo esta listo falta arreglar vista en lista y agregar

--- modulos/articulos/vista/agregar_tipo_egreso.php
+++ modulos/articulos/vista/agregar_tipo_egreso.php
@@ -2,17 +2,11 @@
 
 <form class="formarticulo" id="agregararticulo" method="post" action="" enctype="multipart/form-data" accept-charset="utf-8">
 	<fieldset>
-		<legend>Agregar Nuevo Articulo</legend>
-		<label>Proveedor :</label>
-		<input type="text" id="proveedor" name="proveedor" value="" autofocus required/><br />
-		<label>Articulo en Sist. :</label>
-		<input type="text" id="articulo_sistema" name="articulo_sistema" value="" required/><br />
-		<label>Unidad :</label>
-		<input type="text" id="unidad" name="unidad" required/><br />
-		<label>Articulo en Doc. :</label>
-		<input type="text" id="ariculo_documento" name="articulo_documento" value="" required/><br />
-		<label>Presentación :</label>
-		<input type="text" id="presentacion" name="presentacion" value="" required/><br />
+		<legend>Agregar Nueva Tipología de Egresos</legend>
+		<label>Tipología de Egreso :</label>
+		<input type="text" id="tipo_egreso" name="tipo_egreso" value="" autofocus required/><br />
+		<label>Descripción:</label>
+		<input type="text" id="descripcion" name="descripcion" value="" required/><br />
 		<br />&nbsp;
 		<input type="submit" name="enviar" value="Grabar" />
 		&nbsp; &nbsp;
