d8c6a945ab03476610f2dbc6087141e59f622ff9
jaimematsuda <jaimematsuda@gmail.com>
2015-01-12 15:30:50 -0500

    prueba de codigo python

--- lib/python/actualizar_registros/actualizar_articulos.py
+++ lib/python/actualizar_registros/actualizar_articulos.py
@@ -2,19 +2,51 @@
 # -*- coding: UTF-8 -*-
 
 import pdb
+import pymssql
 import MySQLdb
 
 ip_tienda = '192.168.13.2'
 
-conxguia = MySQLdb.connect('localhost', 'prueba', 'prueba', 'guiaprecios')
+conxinfo = pymssql.connect(
+    host=ip_tienda, user='sa', password='sistemas', database='ALMACEN',
+    as_dict=True)
+
+curalma = conxinfo.cursor()
+
+conxguia = MySQLdb.connect('localhost', 'guiaprecios', 'prat38', 'guiaprecios')
 
 curguia = conxguia.cursor()
 
-query = "SELECT * FROM platos"
+query = "SELECT id_articulo_sistema FROM articulos_sistemas ORDER BY \
+    id_articulo_sistema DESC LIMIT 1"
+
 curguia.execute(query)
 
 for row in curguia:
-    for rs in row:
-        print rs
+    idcodigo = row[0]
 
+query = "SELECT tCodigoProducto, tCodigoSubFamilia, tResumido, \
+    tDetallado, tUnidadEntrada FROM TPRODUCTO \
+    WHERE tCodigoProducto > '%s'" %idcodigo
+
+curalma.execute(query)
+
+for row in curalma:
+    print row['tDetallado']
+"""
+    if row['tCodigoSubFamilia'] == None:
+        idsubfamilia = "1203"
+    else:
+        idsubfamilia = row['tCodigoSubFamilia']
+
+    query = "INSERT INTO articulos_sistemas(id_articulo_sistema, \
+        id_articulo_sub_familia, detallado, resumido, id_unidad) \
+        VALUES('%s', '%s', '%s', '%s', '%s')" %(row['tCodigoProducto'], \
+        idsubfamilia, row['tDetallado'], row['tResumido'], row['tUnidadEntrada'])
+
+    curguia.execute(query)
+    
+conxguia.commit()
+"""
+conxinfo.close()
 conxguia.close()
--- modulos/articulos/vista/actualizar_articulo_sistema.php
+++ modulos/articulos/vista/actualizar_articulo_sistema.php
@@ -1,6 +1,6 @@
 <h2>ARTICULOS DE INFOREST ACTUALIZADOS :</h2>
 <?php
-	foreach ($ as $lista) {
+	foreach ($salida1 as $lista) {
 		echo "<p>".$lista."</p>";
 	}
 ?>
